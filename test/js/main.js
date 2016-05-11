b04c1bc92b218fc07bd55a62af437ed7fa667a7e
neo <15634211355@163.com>
2016-05-11 10:20:30 +0800

    test

---
+++ test/js/main.js
@@ -0,0 +1,37 @@
+/************************************************
+         Author       :  Neo 
+         Date         :2016.5.11
+         Project Name : a part of Car Maintenance 
+     
+************************************************/
+			
+(function(w,d){
+	
+	 var Dom={
+     	
+     };
+     	   
+     function main(){
+     	//整个应用程序的入口函数
+     } 
+          
+     function readerModel(){
+     	//实现和阅读器相关的数据交互的方法
+     	
+     }
+     function readerBaseFrame(){
+     	//渲染基本的UI结构					
+     	
+           
+     }
+     function eventHanlder(){
+     	//交互事件的绑定
+     	   
+     }
+
+
+     main();
+
+
+
+})(window,document);
